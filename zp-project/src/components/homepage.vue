21f5960813eed0f9196e8401491fcba33e610e2a
zhangpeng666 <970517049@qq.com>
2021-02-26 15:06:36 +0800

    feat

--- zp-project/src/components/homepage.vue
+++ zp-project/src/components/homepage.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
     <ul>
       <li>
-        <router-link :to="{ name: 'accumulate' }">
+        <router-link :to="{ name: 'accumulateIndex' }">
           <a>
-            问题积累
+            问题积累-arm64内核解决nvm下载node的问题
           </a>
         </router-link>
       </li>
